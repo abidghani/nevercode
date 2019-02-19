240922b7477bbd5041a249b2e590bec9250ab161
Abid Ghani <abid.ghani@newlook.com>
2019-02-19 15:52:13 +0000

    Initial Commit

---
+++ nevercode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nevercode
+//
+//  Created by Abid_Ghani on 19/02/2019.
+//  Copyright © 2019 Abid_Ghani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
